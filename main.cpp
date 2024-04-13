02484c5afbb40924a525af538f3b12b134e9412e
James Dorevski <github@jamesdorevski.com>
2024-04-14 09:54:00 +1000

    added halt_until_download_complete

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
+
 #include <libtorrent/session.hpp>
 #include <libtorrent/add_torrent_params.hpp>
 #include <libtorrent/magnet_uri.hpp>
+#include <libtorrent/alert_types.hpp>
+#include <libtorrent/torrent_handle.hpp>
+#include <libtorrent/session_params.hpp>
 
 /*
     Features:
@@ -34,6 +40,40 @@ lt::add_torrent_params create_torrent_params(std::string magnet_link, std::strin
     return params;
 }
 
+lt::settings_pack create_settings_pack()
+{
+    lt::settings_pack p;
+    p.set_int(lt::settings_pack::alert_mask, lt::alert_category::status | lt::alert_category::error);
+
+    return p;
+}
+
+int halt_until_download_complete(lt::session& session)
+{
+    bool downloading = true;
+
+    while (downloading) {
+        std::vector<lt::alert*> alerts;
+        session.pop_alerts(&alerts);
+
+        for (lt::alert* alert : alerts) {
+            if (lt::alert_cast<lt::torrent_finished_alert>(alert)) {
+                downloading = false;
+                break;
+            }
+
+            if (lt::alert_cast<lt::torrent_error_alert>(alert)) {
+                std::cerr << "Error downloading torrent. Aborting..." << std::endl;
+                return 3;
+            }
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(5));   
+    }
+
+    return 0;
+}
+
 int main(int argc, char const* argv[])
 {
     int validation = validate_input(argc, argv);
@@ -41,9 +81,17 @@ int main(int argc, char const* argv[])
 
     std::string magnet_link = argv[1];
     std::string media_type = argv[2];
-    std::string path = "/mnt/external-hdd/" + media_type + "/";
+    // std::string path = "/mnt/external-hdd/" + media_type + "/";
+    std::string path = "/home/james/" + media_type + "/";
 
     lt::add_torrent_params params = create_torrent_params(magnet_link, path);
-    
+    lt::settings_pack settings = create_settings_pack();
+    lt::session session(settings);
+
+    lt::torrent_handle handle = session.add_torrent(std::move(params));
+
+    int download_status = halt_until_download_complete(session);
+    if (download_status != 0) return download_status;
+
     return 0;
 }
